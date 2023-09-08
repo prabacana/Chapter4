5f29783f5db17e615f1c5ba46ba6c6eded02f19d
bajipamungkas98@gmail.com <bajipamungkas98@gmail.com>
2023-09-08 18:14:06 +0700

    second initial

---
+++ task.js
@@ -0,0 +1,66 @@
+/**
+ * Tulis sebuah program untuk menampilkan angka genap dari 1 hingga 20
+ */
+
+
+/**
+ * Tulis sebuah program yang menghitung jumlah dari angka 1 hingga 100.
+ */
+
+
+/**
+ * Pola Segitiga Naik
+ * Tampilkan pola segitiga asteriks sebagai berikut untuk n = 5. Contoh:
+
+*
+**
+***
+****
+*****
+
+ */
+
+
+/**
+ * Pola Segitiga Terbalik
+ * Tampilkan pola segitiga asteriks terbalik sebagai berikut untuk n = 5. Contoh:
+ 
+*****
+****
+***
+**
+*
+
+ */
+
+
+/**
+ * Pola Berlian
+ * Tampilkan pola segitiga asteriks berlian sebagai berikut untuk n = 5. Contoh:
+ 
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
+
+ */
+
+
+/**
+ * Pola Segitiga Bilangan
+ * Tampilkan pola segitiga dengan angka dengan asteriks sebagai pemisah:
+
+1
+1*2
+1*2*3
+1*2*3*4
+1*2*3*4*5
+
+ */
+
