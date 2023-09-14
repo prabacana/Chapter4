d240b0f5fe2db948eeaf82040c5d328a7056c9b3
Bayu Aji Pamungkas <bajipamungkas98@gmail.com>
2023-09-14 21:56:25 +0700

    second initial

---
+++ challenge4.js
@@ -0,0 +1,131 @@
+breakVoid = " "
+
+
+/* // Task 1 Done
+
+let arrayFruits = [ 'Apel', 'Mangga', 'Pisang', 'Jeruk' ];
+let arrayAmount = [4, 6, 6, 5];
+
+const arrayConvert = {}
+for (let i = 0; i < arrayFruits.length; i++) {
+    arrayConvert[arrayFruits[i]] = arrayAmount[i]
+}
+
+console.log(arrayConvert);
+
+console.log(breakVoid); */
+
+
+/* //Task 2
+
+let favWord = "saya suka makan nasi goreng dan saya juga suka minum teh"
+
+console.log(breakVoid); */
+
+
+/* //Task 3
+
+const people = [
+    { nama: "Budi", usia: 25 },
+    { nama: "Ana", usia: 20 },
+    { nama: "Citra", usia: 25 },
+    { nama: "Dewi", usia: 32 },
+    { nama: "Eka", usia: 30 }
+  ]
+
+let umurDewi // OUTPUT: 32
+
+let collectionUmur;
+
+console.log(breakVoid); */
+
+
+/* //Task 4 Done
+
+const data = {
+    user: {
+        profile: {
+            name: "John Doe",
+            address: {
+                city: "Jakarta",
+                postalCode: "12345"
+            }
+        }
+    }
+};
+
+const objectCity = data.user.profile.address.city
+
+console.log(objectCity);
+
+console.log(breakVoid); */
+
+
+/* //Task 5
+
+const suara = ['Budi', 'Ana', 'Budi', 'Citra', 'Ana', 'Budi', 'Dewi', 'Dewi', 'Ana'];
+
+console.log(breakVoid); */
+
+
+/* //Task 6 Done
+
+const obj1 = { a: 5, b: 7, c: 3 };
+const obj2 = { a: 4, b: 1, c: 8 };
+const obj3 = { a: 9, b: 2, c: 5 };
+
+const mergingObject = {
+    a: obj1.a + obj2.a + obj3.a,
+    b: obj1.b + obj2.b + obj3.b,
+    c: obj1.c + obj2.c + obj3.c,
+};
+
+console.log(mergingObject);
+
+console.log(breakVoid); */
+
+
+/* //Task7
+
+const orang = {
+    nama: "Budi",
+    alamat: {
+        jalan: "Jl. Raya",
+        kota: "Jakarta",
+        kodePos: "10000"
+    },
+    kontak: {
+        email: "budi@mail.com",
+        telepon: "080080080"
+    }
+};
+
+console.log(orang);
+
+console.log(breakVoid); */
+
+
+/* //Task 8
+
+const mahasiswa = [
+    { nama: "Budi", nilai: 85 },
+    { nama: "Ana", nilai: 90 },
+    { nama: "Dono", nilai: 78 },
+    { nama: "Dewi", nilai: 92 }
+];
+
+console.log(breakVoid); */
+
+
+/* //Task 9
+
+const kota = {
+    Jakarta: 10,
+    Bandung: 5,
+    Surabaya: 7,
+    Medan: 4
+};
+
+console.log(breakVoid); */
+
+
