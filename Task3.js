8552542e9160a17d96af42feb8cdd87571859393
bajipamungkas98@gmail.com <bajipamungkas98@gmail.com>
2023-09-22 20:07:32 +0700

    first initial

---
+++ Task3.js
@@ -0,0 +1,83 @@
+/**
+ * Hapus item terkhir
+ * 
+ * let fruits = ['apple', 'banana', 'cherry']
+ */
+
+let fruits = ['apple', 'banana', 'cherry']
+console.log(fruits);
+// Output: ['apple', 'banana']
+
+
+/**
+ * Cari panjang array
+ * 
+ * Diberikan sebuah array ['a', 'b', 'c', 'd', 'e']. Tentukan panjang dari array tersebut.
+ */
+
+let bulkAlphabet = ['a', 'b', 'c', 'd', 'e']
+console.log(bulkAlphabet);
+// Output: 5
+
+
+/**
+ * Cari nilai tertinggi dan terendah
+ * 
+ * Terdapat array [21, 19, 10, 89, 3, 20, 33, 34], tentukan nilai tertinggi dan nilai terendah. 
+ * Setelah itu urutkan array tersebut tanpa menggunakan built in function
+ */
+let angkaUnik = [21, 19, 10, 89, 3, 20, 33, 34]
+
+
+/**
+ * Gabungkan Dua Array, berikut dengan ketentuan:
+ * 1. Menggunakan built in function
+ * 2. Tidak menggunakan built in function
+ * 
+ * [1, 2, 3] dan [4, 5, 6]
+ */
+let arr1 = [1, 2, 3]
+let arr2 = [3, 4, 5]
+let result1
+console.log(result1);
+// Output : [1, 2, 3, 4, 5, 6]
+
+
+/**
+ * Filter huruf ganjil
+ * 
+ * Diberikan sebuah array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. 
+ * Buatlah sebuah array baru yang hanya berisi angka ganjil dari array tersebut.
+ * 
+ * Ketentuan tanpa menggunakan built in function
+ */
+let arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let angkaGanjil = []
+console.log(angkaGanjil); // [1, 3, 5, 7, 9]
+
+
+/**
+ * Susun ulang array
+ * 
+ * Diberikan array [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]. 
+ * Urutkan array ini dari yang terkecil ke terbesar, kemudian balikkan urutannya. 
+ * Setelah itu, hapus angka yang sama sehingga setiap angka hanya muncul sekali.
+ * 
+ * Bisa menggunakan built in function. Kisi2 sebagai berikut:
+ * 1. Urutkan array terlebih dahulu
+ * 2. Balik urutan array tersebut
+ * 3. Hapus angka yang duplikat
+ */
+
+let angkaKumupulan =[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+// Output : [9, 6, 5, 4, 3, 2, 1]
+
+
+/**
+ * Menggabungkan Array dengan Spesifik
+ * Memiliki dua array, arr1 = [1, 2, 3] dan arr2 = ['a', 'b', 'c']. 
+ * Gabungkan kedua array tersebut sehingga mendapatkan hasil ['a', 1, 'b', 2, 'c', 3].
+ */
+
+let kumpulanAngka = [1, 2, 3]
+let kumpulanHuruf = ["a", "b", "c"]
